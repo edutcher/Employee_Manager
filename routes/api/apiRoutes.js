c9cd0f0905349314551823e0a14290e0bbce9f03
Ethan Dutcher <74162204+edutcher@users.noreply.github.com>
2021-03-10 01:08:28 -0500

    db work and api routes

--- routes/api/apiRoutes.js
+++ routes/api/apiRoutes.js
@@ -1,7 +1,35 @@
+const { json } = require('express');
 const express = require('express');
 const empDB = require('../../lib/empDB');
 const router = express.Router();
 
-router.get("/employees", (req, res) => {})
+let DB = new empDB;
+
+router.get("/employees", async(req, res) => {
+    let peeps = await DB.findAllEmployees();
+    res.json(peeps);
+})
+
+router.get("/roles", async(req, res) => {
+    let roles = await DB.findAllRoles();
+    res.json(roles);
+})
+
+router.get("/departments", async(req, res) => {
+    let dpts = await DB.findAllDepartments();
+    res.json(dpts);
+})
+
+router.get("/employee/:id", async(req, res) => {
+    let { id } = req.params;
+    let peep = await DB.findEmployeeById(id);
+    res.json(peep);
+})
+
+router.get("/department/:id", async(req, res) => {
+    let { id } = req.params;
+    let people = await DB.findEmployeesByDepartment(id);
+    res.json(people);
+})
 
 module.exports = router;
