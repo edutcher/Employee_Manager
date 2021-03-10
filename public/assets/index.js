82994c421eb83ba2da6f979bed59162e6351908f
Ethan Dutcher <74162204+edutcher@users.noreply.github.com>
2021-03-10 16:58:13 -0500

    more db work

--- public/assets/index.js
+++ public/assets/index.js
@@ -1,3 +1,13 @@
-function testy() {
-    console.log('stuff!');
-}
+$(document).ready(() => {
+    axios.get('./api/employees')
+        .then(res => {
+            console.log(res);
+            for (let emp of res.data) {
+                let newRow = $('<tr>');
+                let newName = $('<td>').text(`${emp.first_name} ${emp.last_name}`);
+                let newRole = $('<td>').text(`${emp.role_id}`);
+                newRow.append(newName, newRole);
+                $('#empTable').append(newRow);
+            }
+        })
+});
