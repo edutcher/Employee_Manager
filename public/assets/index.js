9231febd9c0cd55069864a6c71a11480847f77dd
Ethan Dutcher <74162204+edutcher@users.noreply.github.com>
2021-03-10 22:44:24 -0500

    search functional

--- public/assets/index.js
+++ public/assets/index.js
@@ -1,15 +1,42 @@
-$(document).ready(() => {
+function addEmpToTable(emp) {
+    let newRow = $('<tr>');
+    let newName = $('<td>').text(`${emp.first_name} ${emp.last_name}`);
+    let newRole = $('<td>').text(`${emp.title}`);
+    let newSalary = $('<td>').text(`${emp.salary}`);
+    let newDpt = $('<td>').text(`${emp.name}`);
+    newRow.append(newName, newRole, newSalary, newDpt);
+    $('#empTable').append(newRow);
+}
+
+
+function getAllEmployees() {
+    $('#empTable').empty();
     axios.get('./api/employees')
         .then(res => {
-            console.log(res);
             for (let emp of res.data) {
-                let newRow = $('<tr>');
-                let newName = $('<td>').text(`${emp.first_name} ${emp.last_name}`);
-                let newRole = $('<td>').text(`${emp.title}`);
-                let newSalary = $('<td>').text(`${emp.salary}`);
-                let newDpt = $('<td>').text(`${emp.name}`);
-                newRow.append(newName, newRole, newSalary, newDpt);
-                $('#empTable').append(newRow);
+                addEmpToTable(emp);
             }
         })
+}
+
+function empSearch(first, last) {
+    $('#empTable').empty();
+    axios.post('./api/employee/search', { firstName: first, lastName: last })
+        .then(res => {
+            for (let emp of res.data) {
+                addEmpToTable(emp);
+            }
+        })
+}
+
+$(document).ready(() => {
+
+    $('form').submit((e) => {
+        e.preventDefault();
+        if ($('#firstName').val() != '' || $('#lastName').val() != '') {
+            empSearch($('#firstName').val(), $('#lastName').val());
+        }
+    })
+
+    getAllEmployees();
 });
--- routes/api/apiRoutes.js
+++ routes/api/apiRoutes.js
@@ -26,10 +26,22 @@ router.get("/employee/:id", async(req, res) => {
     res.json(peep);
 })
 
+router.post("/employee/search", async(req, res) => {
+    let search = req.body;
+    let peep = await DB.searchEmployee(search);
+    res.json(peep);
+})
+
 router.get("/department/:id", async(req, res) => {
     let { id } = req.params;
     let people = await DB.findEmpsByDepartment(id);
     res.json(people);
+})
+
+router.post("/employee", async(req, res) => {
+    let newEmp = req.body;
+    let peep = await DB.addEmployee(newEmp);
+    res.json(peep);
 })
 
 module.exports = router;
