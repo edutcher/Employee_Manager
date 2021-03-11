cef4b865eb57a1fe9499f3601f19c41f6a125e08
Ethan Dutcher <74162204+edutcher@users.noreply.github.com>
2021-03-10 23:52:30 -0500

    department select

--- public/assets/index.js
+++ public/assets/index.js
@@ -8,6 +8,15 @@ function addEmpToTable(emp) {
     $('#empTable').append(newRow);
 }
 
+function getEmpsByDepartment(id) {
+    $('#empTable').empty();
+    axios.get(`./api/department/${id}`)
+        .then(res => {
+            for (let emp of res.data) {
+                addEmpToTable(emp);
+            }
+        })
+}
 
 function getAllEmployees() {
     $('#empTable').empty();
@@ -29,6 +38,30 @@ function empSearch(first, last) {
         })
 }
 
+function popDropdowns() {
+    $('#dptDropdown').addClass('loading');
+    let newItem = $('<div>').addClass('item').text(`All`);
+    newItem.data('id', 1);
+    $('#deptDrop').append(newItem);
+    axios.get('./api/departments')
+        .then(res => {
+            for (let dept of res.data) {
+                let newItem = $('<div>').addClass('item').text(`${dept.name}`);
+                newItem.data('id', dept.id);
+                $('#deptDrop').append(newItem);
+            }
+            $('#dptDropdown').removeClass('loading');
+            $('#dptDropdown').dropdown();
+            $('.dropdown')
+                .dropdown({
+                    onChange: function(value, text, $selectedItem) {
+                        if ($selectedItem.data('id') === 1) getAllEmployees();
+                        else getEmpsByDepartment($selectedItem.data('id'));
+                    }
+                });
+        });
+}
+
 $(document).ready(() => {
 
     $('form').submit((e) => {
@@ -38,5 +71,14 @@ $(document).ready(() => {
         }
     })
 
+    $('.dropdown')
+        .dropdown({
+            onChange: function(value, text, $selectedItem) {
+                if ($selectedItem.data('id') === 1) getAllEmployees();
+                else getEmpsByDepartment($selectedItem.data('id'));
+            }
+        });
+
     getAllEmployees();
+    popDropdowns();
 });
