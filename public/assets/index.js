6c57e4b2e15778d72ecb76709d9c3b3463540ba7
Ethan Dutcher <74162204+edutcher@users.noreply.github.com>
2021-03-12 22:15:16 -0500

    update employee

--- public/assets/index.js
+++ public/assets/index.js
@@ -1,8 +1,11 @@
 var currentRoles = [];
+var currentManagers = [];
 
 function addEmpToTable(emp) {
     let newRow = $('<tr>');
-    let newName = $('<td>').text(`${emp.first_name} ${emp.last_name}`);
+    let newAnchor = $('<a>').text(`${emp.first_name} ${emp.last_name}`).addClass('emp').data('id', emp.id);
+    let newName = $('<td>');
+    newName.append(newAnchor);
     let newRole = $('<td>').text(`${emp.title}`);
     let newSalary = $('<td>').text(`${emp.salary}`);
     let newDept = $('<td>').text(`${emp.name}`);
@@ -32,6 +35,8 @@ function getAllEmployees() {
             for (let emp of res.data) {
                 addEmpToTable(emp);
             }
+            $('#deptMenu').removeClass('hidden');
+            $('#roleMenu').addClass('hidden');
         }).catch(err => {
             console.log(err);
         })
@@ -44,6 +49,8 @@ function empSearch(first, last) {
             for (let emp of res.data) {
                 addEmpToTable(emp);
             }
+            $('#deptMenu').removeClass('hidden');
+            $('#roleMenu').addClass('hidden');
         }).catch(err => {
             console.log(err);
         })
@@ -70,6 +77,8 @@ function popDeptDropdowns() {
                         else getEmpsByDepartment($selectedItem.data('id'));
                         $('#deptChoice').text(`Department: ${text}`);
                         $('#deptChoice').data('id', value);
+                        $('#deptMenu').addClass('hidden');
+                        $('#roleMenu').removeClass('hidden');
                     }
                 });
         }).catch(err => {
@@ -115,6 +124,7 @@ function popManagerDropdown() {
     if (id === undefined) return;
     axios.get(`./api/employees/managers/${id}`)
         .then(res => {
+            currentManagers = res.data;
             let managers = [];
             for (let manager of res.data) {
                 let newName = manager.first_name + ' ' + manager.last_name;
@@ -136,6 +146,7 @@ function popManagerDropdown() {
 
 function addEmployee() {
     if ($('#newFirstName').val() === '' || $('#newLastName').val() === '' || $('#roleChoice').data('id') === undefined) {
+        $('#newEmpErr').removeClass('hidden');
         return;
     }
     let managerID;
@@ -151,21 +162,57 @@ function addEmployee() {
     axios.post(`./api/employee`, newEmp)
         .then(res => {
             empSearch(newEmp.firstName, newEmp.lastName);
-            $('.ui.modal').modal('hide');
-            $('#newFirstName').val('');
-            $('#newLastName').val('');
-            $('#roleChoice').text('Choose Role');
-            $('#roleChoice').data('id', undefined);
-            $('#managerChoice').text('Choose Manager');
-            $('#managerChoice').data('id', undefined);
+            resetUI();
             $('#deptChoice').text('Choose Department');
             $('#deptChoice').data('id', undefined);
+            $('#deptMenu').removeClass('hidden');
+            $('#roleMenu').addClass('hidden');
         }).catch(err => {
             console.log(err);
         });
 }
 
+function resetUI() {
+    $('.ui.modal').modal('hide');
+    $('#newFirstName').val('');
+    $('#newLastName').val('');
+    $('#roleChoice').text('Choose Role');
+    $('#roleChoice').data('id', undefined);
+    $('#managerChoice').text('Choose Manager');
+    $('#managerChoice').data('id', undefined);
+    $('#newEmpErr').addClass('hidden');
+}
 
+function deleteEmployee() {
+
+}
+
+function updateEmployee() {
+    if ($('#newFirstName').val() === '' || $('#newLastName').val() === '' || $('#roleChoice').data('id') === undefined) {
+        $('#newEmpErr').removeClass('hidden');
+        return;
+    }
+    if (!$('#modalTitle').data('id')) return;
+    let id = $('#modalTitle').data('id');
+    let managerID;
+    if ($('#managerChoice').data('id') === undefined) managerID = null;
+    else managerID = parseInt($('#managerChoice').data('id'));
+    let updatedEmp = {
+        firstName: $('#newFirstName').val(),
+        lastName: $('#newLastName').val(),
+        roleId: parseInt($('#roleChoice').data('id')),
+        isManager: $('.checkbox').checkbox('is checked'),
+        managerId: managerID
+    }
+    axios.put(`./api/employee/${id}`, updatedEmp)
+        .then(res => {
+            if ($('#deptChoice').data('id') === undefined) getAllEmployees();
+            else getEmpsByDepartment($('#deptChoice').data('id'));
+            resetUI();
+        }).catch(err => {
+            console.log(err);
+        });
+}
 
 $(document).ready(() => {
 
@@ -176,6 +223,33 @@ $(document).ready(() => {
         }
     })
 
+    $(document).on('click', '.emp', function(e) {
+        let id = $(this).data('id')
+        $('#modalTitle').data('id', id);
+        $('#modalTitle').text('Update Employee');
+        $('#addButton').addClass('hidden').removeClass('ui');
+        $('#delButton').removeClass('hidden').addClass('ui');
+        $('#updateButton').removeClass('hidden').addClass('ui');
+        popRoleDropdown();
+        axios.get(`./api/employee/${id}`)
+            .then(res => {
+                $('#newFirstName').val(`${res.data[0].first_name}`);
+                $('#newLastName').val(`${res.data[0].last_name}`);
+                $('#roleChoice').text(`Role: ${res.data[0].title}`);
+                for (let role of currentRoles) {
+                    if (role.title === res.data[0].title) $('#roleChoice').data('id', role.id);
+                }
+                $('#managerChoice').text(`Manager: ${res.data[0].manager}`);
+
+                for (let man of currentManagers) {
+                    if ((man.first_name + " " + man.last_name) === res.data[0].manager) $('#managerChoice').data('id', man.id);
+                }
+                popManagerDropdown();
+                $('#newEmpErr').addClass('hidden');
+                $('.ui.modal').modal('show');
+            })
+    })
+
     $('.ui.dropdown')
         .dropdown({
             allowAdditions: true
@@ -183,13 +257,23 @@ $(document).ready(() => {
 
     $('#newEmpForm').submit((e) => {
         e.preventDefault();
-        addEmployee();
+        if ($('#modalTitle').data('id') === null) addEmployee();
+        else updateEmployee()
     })
 
     $('#addEmp').click(() => {
+        $('#modalTitle').data('id', null);
+        $('#modalTitle').text('Add Employee');
+        $('#addButton').removeClass('hidden').addClass('ui');
+        $('#delButton').addClass('hidden').removeClass('ui');
+        $('#updateButton').addClass('hidden').removeClass('ui');
         popRoleDropdown();
         $('#managerDropdown').dropdown();
         $('.ui.modal').modal('show');
+    })
+
+    $('#delButton').click(() => {
+        deleteEmployee();
     })
 
     getAllEmployees();
